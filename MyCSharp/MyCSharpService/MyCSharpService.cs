4015e40f0637c5e328a5e7cddd3e8fbbf1b7f2fa
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2020-09-20 17:51:34 +0900

    C# 프로젝트

---
+++ MyCSharp/MyCSharpService/MyCSharpService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace MyCSharpService
+{
+    class MyCSharpService
+    {
+    }
+}
