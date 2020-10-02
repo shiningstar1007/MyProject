a535bc7d03f50369407334c98f257ab5a0d7f7fb
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2020-10-02 16:33:55 +0900

    FilterMessage

--- MyCSharp/MyCSharpService/MyCSharpService.cs
+++ MyCSharp/MyCSharpService/MyCSharpService.cs
@@ -9,6 +9,37 @@ using System.Security.AccessControl;
 
 namespace MyCSharp.Service
 {
+    public class Win32API
+    {
+        [DllImport("fltlib", SetLastError = true)]
+        public static extern int FilterConnectCommunicationPort (
+            [MarshalAs (UnmanagedType.LPWStr)]
+            string portName,
+            uint options,
+            IntPtr context,
+            uint sizeOfContext,
+            IntPtr securityAttributes,
+            IntPtr hPort
+        );
+
+        [DllImport("fltlib", SetLastError = true)]
+        public static extern int FilterGetMessage (
+            IntPtr hPort,
+            IntPtr msgBuffer,
+            uint msgBufSize,
+            IntPtr lpOverlapped
+        );
+
+        [DllImport("fltlib", SetLastError = true)]
+        public static extern int FilterSendMessage (
+            IntPtr hPort,
+            IntPtr inBuffer,
+            uint inBufferSize,
+            IntPtr outBuffer,
+            uint outBufferSize,
+            uint bytesReturned
+        );
+    }
 
     [ServiceContract()]
     public interface IMyCSharpService
