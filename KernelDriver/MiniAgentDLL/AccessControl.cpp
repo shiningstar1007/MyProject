d14fdf2338d19ad875a739fb79641851f9b06469
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2021-03-07 12:50:48 +0900

    class

--- KernelDriver/MiniAgentDLL/AccessControl.cpp
+++ KernelDriver/MiniAgentDLL/AccessControl.cpp
@@ -1,23 +1,51 @@
+#include <Windows.h>
+#include <string>
+using namespace std;
 
 
-TYPE_SUBJECT StrToSubType(
-	_In_ PCHAR SubTypeStr
-)
-{
-	if (!_stricmp(SubTypeStr, SUBJECT_USER_STR)) return SUBJECT_USER;
-	else if (!_stricmp(SubTypeStr, SUBJECT_GROUP_STR)) return SUBJECT_GROUP;
-	else if (!_stricmp(SubTypeStr, SUBJECT_PROC_STR)) return SUBJECT_PROC;
+class AccessControl {
 
-	return SUBJECT_UNKNOWN;
-}
+public :
+	enum class TYPE_OBJECT {
+		OBJECT_FILE = 0,
+		OBJECT_DIR,
+		OBJECT_UNKNOWN
+	};
 
-PCHAR SubTypeToStr(
-	_In_ TYPE_SUBJECT SubType
-)
-{
-	if (SubType == SUBJECT_USER) return SUBJECT_USER_STR;
-	if (SubType == SUBJECT_GROUP) return SUBJECT_GROUP_STR;
-	else if (SubType == SUBJECT_PROC) return SUBJECT_PROC_STR;
+	const string OBJECT_FILE_STR = "file";
+	const string OBJECT_DIR_STR = "dir";
 
-	return NULL;
-}
+	enum class TYPE_SUBJECT {
+		SUBJECT_USER = 0,
+		SUBJECT_GROUP,
+		SUBJECT_PROC,
+		SUBJECT_UNKNOWN
+	};
+
+	const string SUBJECT_USER_STR = "user";
+	const string SUBJECT_GROUP_STR = "group";
+	const string SUBJECT_PROC_STR = "proc";
+
+
+	TYPE_SUBJECT StrToSubType(
+		string SubTypeStr
+	)
+	{
+		if (!SubTypeStr.compare(SUBJECT_USER_STR)) return TYPE_SUBJECT::SUBJECT_USER;
+		else if (!SubTypeStr.compare(SUBJECT_GROUP_STR)) return TYPE_SUBJECT::SUBJECT_GROUP;
+		else if (!SubTypeStr.compare(SUBJECT_PROC_STR)) return TYPE_SUBJECT::SUBJECT_PROC;
+
+		return TYPE_SUBJECT::SUBJECT_UNKNOWN;
+	}
+
+	string SubTypeToStr(
+		TYPE_SUBJECT SubType
+	)
+	{
+		if (SubType == TYPE_SUBJECT::SUBJECT_USER) return SUBJECT_USER_STR;
+		if (SubType == TYPE_SUBJECT::SUBJECT_GROUP) return SUBJECT_GROUP_STR;
+		else if (SubType == TYPE_SUBJECT::SUBJECT_PROC) return SUBJECT_PROC_STR;
+
+		return "";
+	}
+};
