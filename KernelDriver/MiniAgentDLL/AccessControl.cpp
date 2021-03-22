d36460af13e87f42857c871c57acdca863c8980e
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2021-03-23 08:58:33 +0900

    split

--- KernelDriver/MiniAgentDLL/AccessControl.cpp
+++ KernelDriver/MiniAgentDLL/AccessControl.cpp
@@ -1,7 +1,25 @@
 #include "AccessControl.h"
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
 
+using namespace std;
 
-using std::string;
+template <typename Out>
+void mySplit(const string& s, char delim, Out result) {
+	istringstream iss(s);
+	string item;
+	while (getline(iss, item, delim)) {
+		*result++ = item;
+	}
+}
+
+vector<string> split(const string& s, char delim) {
+	vector<string> elems;
+	split(s, delim, back_inserter(elems));
+	return elems;
+}
 
 class AccessControl {
 
