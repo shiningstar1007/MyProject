c9ecc088438202f67c294c4a04a08fde57a27018
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2021-04-05 20:11:54 +0900

    ACL

--- KernelDriver/MiniComm/MiniComm.cpp
+++ KernelDriver/MiniComm/MiniComm.cpp
@@ -44,3 +44,61 @@ BOOL GetWinVer(DWORD& dwMajor, DWORD& dwMinor)
 
 	return ret;
 }
+
+HRESULT SendMessageDriver(MINI_COMMAND Cmd, PBYTE InByte, ULONG InSize, PBYTE OutBuf, ULONG OutSize, PULONG Result)
+{
+	HRESULT hResult = S_OK;
+	HANDLE MiniPort = INVALID_HANDLE_VALUE;
+	PCOMMAND_MESSAGE CmdMsg = NULL;
+	PCHAR Buffer = NULL;
+	DWORD ByteResult = 0, BufSize = 0;
+
+	__try {
+		hResult = FilterConnectCommunicationPort(MINI_PORT_NAME, 0, NULL, 0, NULL, &MiniPort);
+		if (IS_ERROR(hResult)) {
+
+			__leave;
+		}
+
+		if (OutBuf) {
+			Buffer = (PCHAR)LocalAlloc(LMEM_FIXED, MAX_REG_VALUE_LEN);
+			if (!Buffer) {
+				hResult = HRESULT_FROM_WIN32(GetLastError());
+
+				__leave;
+			}
+			else memset(Buffer, 0, MAX_REG_VALUE_LEN);
+
+			BufSize = MAX_REG_VALUE_LEN;
+		}
+
+
+		CmdMsg = (PCOMMAND_MESSAGE)LocalAlloc(LMEM_FIXED, sizeof(COMMAND_MESSAGE) + InSize);
+		if (!CmdMsg) __leave;
+		else memset(CmdMsg, 0, sizeof(COMMAND_MESSAGE) + InSize);
+
+		CmdMsg->Command = Cmd;
+		if (InByte) memcpy(CmdMsg->Data, InByte, InSize);
+
+		hResult = FilterSendMessage(MiniPort, CmdMsg, sizeof(COMMAND_MESSAGE) + InSize, Buffer, BufSize, &ByteResult);
+		if (IS_ERROR(hResult)) __leave;
+
+		if (OutBuf && ByteResult > 0) {
+			ByteResult = min(OutSize, ByteResult);
+			memcpy(OutBuf, Buffer, ByteResult);
+
+			if (Result) *Result = ByteResult;
+		}
+
+	}
+	__finally
+	{
+		if (CmdMsg) LocalFree(CmdMsg);
+
+		if (Buffer) LocalFree(Buffer);
+
+		if (MiniPort != INVALID_HANDLE_VALUE) CloseHandle(MiniPort);
+	}
+
+	return hResult;
+}
