8feb7fed2bdea1f677790f711c97e1df3c91ba3b
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2021-09-27 19:06:01 +0900

    ACL

---
+++ MyCSharp/MyCSharpService/CommFunc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpService
+{
+    public enum SUB_TYPE
+    {
+        SUB_USER = 0,
+        SUB_PROC,
+        SUB_SHARE,
+        SUB_GROUP,
+        SUB_UNKNOWN
+    }
+
+    public static class SUB_TYPE_STR
+    {
+        public const String SUB_USER_STR = "user";
+        public const String SUB_GROUP_STR = "group";
+        public const String SUB_PROC_STR = "proc";
+        public const String SUB_SHARE_STR = "share";
+    }
+    class CommFunc
+    {
+        public SUB_TYPE StrToSubType(String SubTypeStr)
+        {
+            if (SubTypeStr == SUB_TYPE_STR.SUB_USER_STR) return SUB_TYPE.SUB_USER;
+            else if (SubTypeStr == SUB_TYPE_STR.SUB_PROC_STR) return SUB_TYPE.SUB_PROC;
+            else if (SubTypeStr == SUB_TYPE_STR.SUB_SHARE_STR) return SUB_TYPE.SUB_SHARE;
+            else if (SubTypeStr == SUB_TYPE_STR.SUB_GROUP_STR) return SUB_TYPE.SUB_GROUP;
+
+            return SUB_TYPE.SUB_UNKNOWN;
+        }
+    }
+}
