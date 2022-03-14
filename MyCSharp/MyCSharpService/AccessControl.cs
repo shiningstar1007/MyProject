0b96281b1e17520f53cdd8db0393bc54db6b4299
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2022-03-14 23:05:20 +0900

    ACL

--- MyCSharp/MyCSharpService/AccessControl.cs
+++ MyCSharp/MyCSharpService/AccessControl.cs
@@ -1,11 +1,9 @@
 ﻿using MyCSharp.Service;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MyCSharpService
 {
