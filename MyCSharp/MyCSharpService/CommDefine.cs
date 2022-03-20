78714933cd4180cbc6a88c62a06d1b98b78137f5
DESKTOP-BR5HF1G\yunseong <blackreport5@gmail.com>
2022-03-20 22:50:33 +0900

    ACL

--- MyCSharp/MyCSharpService/CommDefine.cs
+++ MyCSharp/MyCSharpService/CommDefine.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IMyCSharpService
+﻿namespace IMyCSharpService
 {
     public static class RegDefine
     {
