a7e81e275e770400faccccbe3c8a9f792f47653f
DESKTOP-BR5HF1G\yunseong <blackreport5@gmail.com>
2022-03-21 23:55:28 +0900

    ACL

--- MyCSharp/MyCSharpService/MyCSharpService.cs
+++ MyCSharp/MyCSharpService/MyCSharpService.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.ServiceModel;
-using System.Runtime.InteropServices;
-using System.IO;
-using System.Security.AccessControl;
-using System.Text.RegularExpressions;
-using System.Linq;
-using System.Diagnostics;
-using System.Net;
-using System.Net.Sockets;
-using System.Management;
-using System.Security.Principal;
-using Microsoft.Win32;
-using IMyCSharpService;
+﻿using System.ServiceModel;
 
 namespace MyCSharp.Service
 {
