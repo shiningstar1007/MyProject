7a76a36a0bbff559eb6c5391f46a7922e41314a0
DESKTOP-BR5HF1G\yunseong <blackreport5@gmail.com>
2022-04-23 20:46:53 +0900

    ACL

--- MyCSharp/MyCSharpService/CommFunc.cs
+++ MyCSharp/MyCSharpService/CommFunc.cs
@@ -248,7 +248,7 @@ namespace IMyCSharpService
 
             return strKey;
         }
-        public Boolean CheckProcessExt(String procExt)
+        public static Boolean CheckProcessExt(String procExt)
         {
             Boolean bExeFile = false;
             String buffer;
